0810c215b6852bdf2b8548fe8b61ef9219157d69
samuel junior queiroz lisboa <141160028+samueljuni@users.noreply.github.com>
2025-03-10 08:46:40 -0300

    Add files via upload

---
+++ samuel 3/script.js
@@ -0,0 +1,15 @@
+const alternaContraste = document.getElementById('alterna-contraste')
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+const mapaVisivel = botaoMapa.getAttribute('aria-expanded') === 'true';
+<script src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.js"></script>
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
+vvv
